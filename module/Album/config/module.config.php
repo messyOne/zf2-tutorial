b55fc8c5ad2b58a68e102548d1876b9b5d009d77
messy <mail@weiss-daniel.de>
2012-09-16 01:45:15 +0200

    Added module.config.php

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,14 @@
+<?php
+// module/Album/conﬁg/module.config.php:
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);
